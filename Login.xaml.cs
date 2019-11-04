572954b86c118c7593bfa1fd08ac0f8ff69ea83d
DrCamf <drcamf@gmail.com>
2019-11-04 14:34:01 +0100

    Last update of the day just gui stuff

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,13 +14,18 @@ using System.Windows.Shapes;
 namespace library01
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class Login : Page
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Test.xaml.cs
+++ Test.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +14,11 @@ using System.Windows.Shapes;
 namespace library01
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Test.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class Test : Page
     {
-        public MainWindow()
+        public Test()
         {
             InitializeComponent();
         }
